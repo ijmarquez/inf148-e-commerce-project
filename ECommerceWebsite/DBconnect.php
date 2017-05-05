349e4575d63821e044b8e7f1ae8d9850214cf370
cpoon4 <poonch@uci.edu>
2017-05-05 16:54:31 -0700

    php CONNECTS! FUCK

--- ECommerceWebsite/DBconnect.php
+++ ECommerceWebsite/DBconnect.php
@@ -1,21 +1,26 @@
 <?php
-    include $_SERVER['DOCUMENT_ROOT'].'/../dbconfig.php';;
-    $hostname = DB_HOST;
-    $username = DB_USER;
-    $password = DB_PASSWORD;
-    $dbname = DB_DATABASE;
+//include $_SERVER['DOCUMENT_ROOT'].'/../dbconfig.php';
+//    $hostname = DB_HOST;
+//    $username = DB_USER;
+//    $password = DB_PASSWORD;
+//    $dbname = DB_DATABASE;
+$servername = "sylvester-mccoy-v3.ics.uci.edu";
+$username = "inf124-db-063";
+$password = "GSaxgpMPZKhN";
+$dbname = "inf124-db-063";
 
-    try {
-        echo"inside try \n";
-        $conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
-        echo"afterPDO \n";
-        //set the PDO error to exception
-        $conn->setAttribute(PDO::ATTR_ERRMOD, PDO::ERRMODE_EXCEPTION);
-        echo"it works \n";
-    } catch (PDOException $e) {
-        echo"Username: " .$username;
-        echo"Connection failed: " .$e->getMessage();
-    }
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-    $conn = null;
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
+
+$conn = null;
 ?>
+
